e8e61786975a78c8ce4c85cd91fc4ee7f2bcb82a
novonotes-dev <novonotes.dev@gmail.com>
2026-05-29 03:25:19 +0900

    Avoid downstream clap-sys dependency

--- crates/wrac_clap_adapter/src/lib.rs
+++ crates/wrac_clap_adapter/src/lib.rs
@@ -41,6 +41,27 @@ pub use process_buffer::{
 pub mod __private {
     pub use crate::entry::EntryRegistration;
 
+    #[repr(C)]
+    pub struct ClapVersion {
+        pub major: u32,
+        pub minor: u32,
+        pub revision: u32,
+    }
+
+    #[repr(C)]
+    pub struct ClapPluginEntry {
+        pub clap_version: ClapVersion,
+        pub init: Option<unsafe extern "C" fn(plugin_path: usize) -> bool>,
+        pub deinit: Option<unsafe extern "C" fn()>,
+        pub get_factory: Option<unsafe extern "C" fn(factory_id: usize) -> usize>,
+    }
+
+    pub const CLAP_VERSION: ClapVersion = ClapVersion {
+        major: ::clap_sys::version::CLAP_VERSION.major,
+        minor: ::clap_sys::version::CLAP_VERSION.minor,
+        revision: ::clap_sys::version::CLAP_VERSION.revision,
+    };
+
     pub unsafe fn entry_init(registration: &'static EntryRegistration, plugin_path: usize) -> bool {
         unsafe { crate::abi::entry_init(registration, plugin_path as *const ::std::ffi::c_char) }
     }
@@ -72,30 +93,23 @@ macro_rules! export_clap_entry {
             static WRAC_CLAP_ENTRY_REGISTRATION: $crate::__private::EntryRegistration =
                 $crate::__private::EntryRegistration::new($entry);
 
-            unsafe extern "C" fn wrac_clap_entry_init(
-                plugin_path: *const ::std::ffi::c_char,
-            ) -> bool {
-                $crate::__private::entry_init(&WRAC_CLAP_ENTRY_REGISTRATION, plugin_path as usize)
+            unsafe extern "C" fn wrac_clap_entry_init(plugin_path: usize) -> bool {
+                $crate::__private::entry_init(&WRAC_CLAP_ENTRY_REGISTRATION, plugin_path)
             }
 
             unsafe extern "C" fn wrac_clap_entry_deinit() {
                 $crate::__private::entry_deinit(&WRAC_CLAP_ENTRY_REGISTRATION)
             }
 
-            unsafe extern "C" fn wrac_clap_entry_get_factory(
-                factory_id: *const ::std::ffi::c_char,
-            ) -> *const ::std::ffi::c_void {
-                $crate::__private::entry_get_factory(
-                    &WRAC_CLAP_ENTRY_REGISTRATION,
-                    factory_id as usize,
-                ) as *const ::std::ffi::c_void
+            unsafe extern "C" fn wrac_clap_entry_get_factory(factory_id: usize) -> usize {
+                $crate::__private::entry_get_factory(&WRAC_CLAP_ENTRY_REGISTRATION, factory_id)
             }
 
             #[allow(unreachable_pub)]
             #[unsafe(no_mangle)]
-            pub static clap_entry: ::clap_sys::entry::clap_plugin_entry =
-                ::clap_sys::entry::clap_plugin_entry {
-                    clap_version: ::clap_sys::version::CLAP_VERSION,
+            pub static clap_entry: $crate::__private::ClapPluginEntry =
+                $crate::__private::ClapPluginEntry {
+                    clap_version: $crate::__private::CLAP_VERSION,
                     init: Some(wrac_clap_entry_init),
                     deinit: Some(wrac_clap_entry_deinit),
                     get_factory: Some(wrac_clap_entry_get_factory),
@@ -103,8 +117,8 @@ macro_rules! export_clap_entry {
 
             #[allow(unreachable_pub)]
             #[unsafe(no_mangle)]
-            pub extern "C" fn get_clap_entry() -> *const ::clap_sys::entry::clap_plugin_entry {
-                &clap_entry
+            pub extern "C" fn get_clap_entry() -> usize {
+                (&clap_entry as *const $crate::__private::ClapPluginEntry) as usize
             }
         }
     };
